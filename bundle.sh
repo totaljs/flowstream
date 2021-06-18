10244cbce243723bbf090ec4162e6aa5ac38057b
Peter Širka <petersirka@gmail.com>
2021-06-18 14:21:25 +0200

    Fixed bundles.

--- bundle.sh
+++ bundle.sh
@@ -3,6 +3,7 @@ cd .bundle
 cp -a ../controllers controllers
 cp -a ../definitions definitions
 cp -a ../public public
+cp -a ../modules modules
 cp -a ../schemas schemas
 cp -a ../views views
 total4 --bundle app.bundle
